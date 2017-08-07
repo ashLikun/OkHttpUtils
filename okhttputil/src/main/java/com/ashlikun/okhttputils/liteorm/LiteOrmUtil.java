e52c95a540b6e12b37efb4defb28f567bcc26915
likun <496546144@qq.com>
2017-08-07 22:55:59 +0800

    init  mothe

--- okhttputil/src/main/java/com/ashlikun/okhttputils/liteorm/LiteOrmUtil.java
+++ okhttputil/src/main/java/com/ashlikun/okhttputils/liteorm/LiteOrmUtil.java
@@ -9,18 +9,57 @@ import com.litesuits.orm.db.DataBaseConfig;
 
 public class LiteOrmUtil {
     private static LiteOrm liteOrm;
-    private static Application app;
-    private static int versionCode;
-    private static boolean debugged;
+
+    private static OnNeedListener listener;
+
+    /**
+     * 作者　　: 李坤
+     * 创建时间: 2017/8/7 10:29
+     * 邮箱　　：496546144@qq.com
+     * <p>
+     * 方法功能：一定要在Application里面调用
+     */
+
+    public static void init(OnNeedListener listener) {
+        LiteOrmUtil.listener = listener;
+    }
+
+
+    public interface OnNeedListener {
+        public Application getApplication();
+
+        public boolean isDebug();
+
+        public int getVersionCode();
+    }
+
+    public static Application getApp() {
+        if (listener == null) {
+            throw new RuntimeException("请在Application调用Utils的init方法");
+        } else {
+            return listener.getApplication();
+        }
+    }
+
+    public static boolean isDebug() {
+        if (listener == null) {
+            throw new RuntimeException("请在Application调用Utils的init方法");
+        } else {
+            return listener.isDebug();
+        }
+    }
+
+    public static int versionCode() {
+        if (listener == null) {
+            throw new RuntimeException("请在Application调用Utils的init方法");
+        } else {
+            return listener.getVersionCode();
+        }
+    }
 
     private LiteOrmUtil() {
     }
 
-    public static void init(Application application, int versionCode, boolean debugged) {
-        LiteOrmUtil.app = application;
-        LiteOrmUtil.versionCode = versionCode;
-        LiteOrmUtil.debugged = debugged;
-    }
 
     public static LiteOrm getLiteOrm() {
         if (liteOrm == null) {
@@ -35,9 +74,10 @@ public class LiteOrmUtil {
 
     private static void init() {
         if (liteOrm == null) {
-            DataBaseConfig config = new DataBaseConfig(app, app.getString(R.string.app_name_letter));
-            config.debugged = debugged;
-            config.dbVersion = versionCode;
+            DataBaseConfig config = new DataBaseConfig(getApp(), getApp().getString(R.string.app_name_letter))
+            ;
+            config.debugged = isDebug();
+            config.dbVersion = versionCode();
             config.onUpdateListener = null;
             liteOrm = LiteOrm.newSingleInstance(config);
         }
