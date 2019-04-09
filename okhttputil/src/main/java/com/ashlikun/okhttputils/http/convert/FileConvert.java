ac7db014d492f5deb61b7bbc03d56082fe7f5b9c
likun <496546144@qq.com>
2019-04-09 10:59:06 +0800

    优化

--- okhttputil/src/main/java/com/ashlikun/okhttputils/http/convert/FileConvert.java
+++ okhttputil/src/main/java/com/ashlikun/okhttputils/http/convert/FileConvert.java
@@ -32,14 +32,13 @@ import okhttp3.Response;
 import okhttp3.ResponseBody;
 
 /**
- * ================================================
- * 作    者：jeasonlzy（廖子尧）Github地址：https://github.com/jeasonlzy
- * 版    本：1.0
- * 创建日期：16/9/11
- * 描    述：字符串的转换器
- * 修订历史：
- * ================================================
+ * @author　　: 李坤
+ * 创建时间: 2018/5/9 10:33
+ * 邮箱　　：496546144@qq.com
+ * <p>
+ * 功能介绍：文件转换器
  */
+
 public class FileConvert implements Converter<File> {
     //下载目标文件夹
     public static final String DM_TARGET_FOLDER = File.separator + "download" + File.separator;
