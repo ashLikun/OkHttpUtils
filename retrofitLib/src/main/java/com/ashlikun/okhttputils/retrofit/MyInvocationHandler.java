109b5f1151756e519d1ab483f34e9f71589fb9e2
likun <496546144@qq.com>
2021-05-08 23:09:50 +0800

    优化

--- retrofitLib/src/main/java/com/ashlikun/okhttputils/retrofit/MyInvocationHandler.java
+++ retrofitLib/src/main/java/com/ashlikun/okhttputils/retrofit/MyInvocationHandler.java
@@ -31,7 +31,7 @@ class MyInvocationHandler implements InvocationHandler {
         if (args != null && args.length > 1) {
             Object continuation = args[args.length - 1];
             if (continuation instanceof Continuation) {
-                retrofit.start(method, args);
+                retrofit.proxyStart(method, args);
                 return retrofit.loadServiceMethod(method).invoke(args, (Continuation) continuation);
             }
         }
